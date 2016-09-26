d88aa9aad9cc994d3d4d9b2ed39592ca7c6bac83
chunyin <chunyin@live.com>
2016-09-26 09:35:32 +0800

    allowed initial value

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,7 @@
 #User variables
-	$monthly = 200 					#How much you're planning to save.	-	-	-	-	(Monthly)
-	$rates = 1.02					#Rate of returns on savings. -	-	-	-	-	-	(Annual )
+  $initial = 0            #How much you start with
+  $monthly = 200 					#How much you're planning to save.	-	-	-	-	(Monthly)
+	$rates = 1.02				  	#Rate of returns on savings. -	-	-	-	-	-	(Annual )
 	$inflation = 1.02				#Rate of inflation. 	-	-	-	-	-	-	(Annual )
 	$years_waiting_before_withdrawing = 20		#How many years you'd like to wait before withdrawing your money.
 	$years_saving_monthly = 10			#How many years would you like to spend deposit money monthly.
@@ -10,7 +11,7 @@
 
 	#System commands
 	$adjusted = 0
-	$final = 0
+	$final = $initial
 	$i = 0
 	$amount_deposited = 0
 	$inflation_now = 1
